dc1d88fcb32ca613656068809739a69ae497eff7
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2024-08-29 17:17:34 +0200

    feat: make compute macros script colorful

--- scripts/compute_macros.py
+++ scripts/compute_macros.py
@@ -4,37 +4,46 @@ from food_manager.utils import get_calories, get_macro_grams
 
 CARB_BUDGET_GRAMS = 20.0
 
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+
+RESET = "\033[0m"
+
 for day_name, daily_meal_plan in WEEKLY_MEAL_PLAN.dailies.items():
     if len(daily_meal_plan.meals) == 0:
         continue
 
+    print(YELLOW, end="")
+    print(f"{'-' * len(day_name)}")
     print(f"{day_name.upper()}")
-    print("-" * 20)
+    print(f"{'-' * len(day_name)}")
+    print(RESET, end="")
     total_calories = 0.0
-    total_carbs = 0.0
+    total_carb = 0.0
     total_fat = 0.0
     total_protein = 0.0
     for meal_type, meal in daily_meal_plan.meals.items():
-        print(f"{meal_type.capitalize()}:")
+        print(f"{YELLOW}{meal_type.capitalize()}:{RESET}")
         for food in meal.foods.values():
             macro_grams = get_macro_grams(food)
             calories = get_calories(macro_grams)
 
+            food_name = food.type_.as_named().name
             carb_budget = macro_grams.carb / CARB_BUDGET_GRAMS * 100
-            print(f"  {food.type_.as_named().name}: {calories:.0f} calories")
-            print(f"    {macro_grams.carb:6.1f}g carbs ({carb_budget:3.0f}%)")
+            print(f"  {GREEN}{food_name}{RESET}: {calories:.0f} calories")
+            print(f"    {macro_grams.carb:6.1f}g carb ({carb_budget:3.0f}%)")
             print(f"    {macro_grams.fat:6.1f}g fat")
             print(f"    {macro_grams.protein:6.1f}g protein")
 
             total_calories += calories
-            total_carbs += macro_grams.carb
+            total_carb += macro_grams.carb
             total_fat += macro_grams.fat
             total_protein += macro_grams.protein
 
-    total_carb_budget = total_carbs / CARB_BUDGET_GRAMS * 100
-    print("-" * 20)
-    print(f"  Total: {total_calories:.0f} calories")
-    print(f"    {total_carbs:6.1f}g carbs ({total_carb_budget:3.0f}%)")
+    total_carb_budget = total_carb / CARB_BUDGET_GRAMS * 100
+    print()
+    print(f"Total: {total_calories:.0f} calories")
+    print(f"    {total_carb:6.1f}g carb ({total_carb_budget:3.0f}%)")
     print(f"    {total_fat:6.1f}g fat")
     print(f"    {total_protein:6.1f}g protein")
     print()
